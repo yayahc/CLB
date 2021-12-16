3e4b055fcde33dd93f57771de97aaf1e9b69c992
Yaya HC <hamedcuenca5@gmail.com>
2021-12-16 02:10:54 +0000

    re-fonte

--- clb_django_app/produit/admin.py
+++ clb_django_app/produit/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Produit, Album, Categorie, Vendeur
+from .models import Produit, Categorie, Vendeur
 
 admin.site.register(Produit)
-admin.site.register(Album)
+# admin.site.register(Album)
 admin.site.register(Categorie)
 admin.site.register(Vendeur)
--- clb_django_app/produit/views.py
+++ clb_django_app/produit/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
-from .models import Produit, Album, Categorie, Vendeur
+from .models import Produit, Categorie, Vendeur
 
 
 def categories(request):
@@ -12,16 +12,16 @@ def categories(request):
 
 def home(request):
     produit = Produit.objects.filter(in_home=True)
-    return render(request, 'produit/vendor/home-page.html', {'produit':produit})
+    return render(request, 'pages/home.html', {'produit':produit})
 
 def detail(request, slug):
     produit = get_object_or_404(Produit, slug=slug, in_stock=True)
-    return render(request, 'produit/vendor/product-page.html', {'produit':produit})
+    return render(request, 'pages/detail.html', {'produit':produit})
 
 def categorie_list(request, categorie_slug):
     categorie = get_object_or_404(Categorie, slug=categorie_slug)
     produit = Produit.objects.filter(categorie=categorie)
-    return render(request, 'produit/vendor/categorie.html', {'produit':produit, 'categorie':categorie})
+    return render(request, 'pages/categorie.html', {'produit':produit, 'categorie':categorie})
 
 def achat(request):
     return render(request, 'produit/achat.html')
